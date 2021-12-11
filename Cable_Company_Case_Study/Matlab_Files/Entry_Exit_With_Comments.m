4ef8abf2e876c4d40d73c3ef4d12d8eec03d2941
james051898 <74512319+james051898@users.noreply.github.com>
2021-12-11 18:56:52 -0500

    Fixed Entry_Exit model

--- Cable_Company_Case_Study/Matlab_Files/Entry_Exit_With_Comments.m
+++ Cable_Company_Case_Study/Matlab_Files/Entry_Exit_With_Comments.m
@@ -1,15 +1,15 @@
-% MFSC03 Model function file for Production-Adjustment Model
-function out1 = Entry_Exit_With_Comments(flag,s,x,Vs,kappa,alpha,sigma,a,c,rho);
- n = size(s,1);
- switch flag
- case 'x';                          % OPTIMAL CONTROL
-   out1 = Vs(:,2)/a;
- case 'f';                          % REWARD FUNCTION
-   out1 = s(:,1).*s(:,2) - 0.5*c*s(:,2).^2 - 0.5*a*x.^2;
- case 'g';                          % DRIFT FUNCTION
-   out1 = [kappa*(alpha-s(:,1)) x];
- case 'sigma'                       % DIFFUSION FUNCTION
-   out1 = [sigma*s(:,1) zeros(n,3)];
- case 'rho'                         % DISCOUNT FUNCTION
-   out1 = rho+zeros(n,1);
- end
+function out=Entry_Exit_Model_With_Comments(flag,s,x,r,mu,sigma,C,E,I)
+  switch flag
+  case 'f'
+    out=(s-C).*(x==2);
+  case 'g'
+    out=mu*s;
+  case 'sigma'
+    out=sigma*s;
+  case 'rho'
+    out=r+zeros(size(s,1),1);
+  case 'reward'
+    out=[0 0 0;-I 0 0;-E 0 0;0 0 0];
+  otherwise
+    error('invalid flag')
+  end
